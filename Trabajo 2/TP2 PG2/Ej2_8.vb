7fba527df760c012a287ebc65e310eaf361957c8
NishiuchiDaniel <38296946+NishiuchiDaniel@users.noreply.github.com>
2018-05-09 21:08:56 -0300

    Add files via upload

--- Trabajo 2/TP2 PG2/Ej2_8.vb
+++ Trabajo 2/TP2 PG2/Ej2_8.vb
@@ -1,30 +1,45 @@
 ﻿Module Ej2_8
+    Enum Meses As Byte
+        Enero = 1
+        Febrero = 2
+        Marzo = 3
+        Abril = 4
+        Mayo = 5
+        Junio = 6
+        Julio = 7
+        Agosto = 8
+        Septiembre = 9
+        Octubre = 10
+        Noviembre = 11
+        Diciembre = 12
+    End Enum
+
     Sub main()
 
-        Dim mes As Byte
+        Dim mes As Meses
         Dim anio As Int16
-        Dim b As Integer
         Console.Write("Ingrese mes: ")
         mes = Console.ReadLine()
-        If (mes = 2) Then
-            Console.WriteLine("asd")
-        End If
-        Console.WriteLine()
+
+        Select Case mes
+            Case 1, 3, 5, 7, 8, 10, 12
+                Console.WriteLine(mes.ToString & " Tiene 31 dias")
+            Case 4, 6, 9, 11
+                Console.WriteLine(mes.ToString & " Tiene 30 dias")
+            Case 2
+                Console.Write("Ingrese anio: ")
+                anio = Console.ReadLine()
+                If (((mes Mod 4) = 0 And (mes Mod 100) <> 0) Or mes Mod 400 = 0) Then
+                    Console.WriteLine(mes.ToString & " Tiene 29 dias")
+                Else
+                    Console.WriteLine(mes.ToString & "Tiene 28 dias")
+                End If
+            Case Else
+                Console.WriteLine("Fuera de rango")
+        End Select
+
         Console.ReadKey()
     End Sub
 
-    Enum Meses As Byte
-        Enero = 31
-        Febrero = 30
-        Marzo = 31
-        Abril = 30
-        Mayo = 31
-        Junio = 30
-        Julio = 31
-        Agosto = 31
-        Septiembre = 30
-        Octubre = 31
-        Noviembre = 30
-        Diciembre = 31
-    End Enum
+
 End Module
